8866b95bb3600ffe14067ae0d9dda65387b4c036
Michael Juszkiewicz ; 43245 <mikejuszkie@gmail.com>
2016-12-19 17:09:45 -0500

    initial framework for letter by letter challange

--- 295_letter_by_letter/main.c
+++ 295_letter_by_letter/main.c
@@ -21,6 +21,33 @@
 
 int main(int argc, char const *argv[])
 {
-  /* code */
-  return 0;
+
+	int str_len = 0;
+
+  	// Take Input from user or file.
+
+	char str_A[4] = "this";
+	char str_B[4] = "shat";
+
+	// check the length of both strings.
+	str_len = sizeof(str_A);
+	if(str_len < sizeof(str_B))
+		str_len = sizeof(str_B);
+
+	// use a for loop to change the sting 
+	// letter by letter and print out each step.
+	printf("%s\n", str_A);
+	for (int i = 0; i < str_len; ++i)
+	{
+		
+		if(str_A[i] != str_B[i])
+		{
+			str_A[i] = str_B[i];
+			printf("%s\n", str_A);
+		}
+		
+	}
+
+
+  	return 0;
 }
